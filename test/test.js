036517e3a186e2364920c49589837c33c7a1cee0
Pankaj Vaghela <vpankaj1998official@gmail.com>
2020-03-29 16:34:46 +0530

    setup coveralls coverage

--- test/test.js
+++ test/test.js
@@ -2,8 +2,14 @@
 
 var expect = require('chai').expect;
 
+var SequelizeAcl = require('../lib');
+
 describe('basictests', function() {
   it('it should pass basic test', function() {
     expect(1).to.equal(1); 
+  });
+  it('it should return sum of numbers', function() {
+    var a = SequelizeAcl.sum(2,3)
+    expect(a).to.equal(5); 
   });
 });
